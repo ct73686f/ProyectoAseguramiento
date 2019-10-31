ee4135ae1bb905b611bdee09ce4baa5ae405be99
Carlos T. Lap <ct73686f@gmail.com>
2019-10-31 14:14:19 -0600

    Se cambia clave gaby2019 a gaby2020

--- ProyectoAseguramiento/Login.aspx.cs
+++ ProyectoAseguramiento/Login.aspx.cs
@@ -25,7 +25,7 @@ namespace ProyectoAseguramiento
         protected void Button1_Click(object sender, EventArgs e)
         {
             String usuario = "carlos";
-            String contraseña = "gaby2019";
+            String contraseña = "gaby2020";
 
             
             if (txtNombreUsuario.Text.Equals(usuario) && txtContraseña.Text.Equals(contraseña)){
